eacf110ef93f8b48f29f8544563183e12d6eb2a7
diaszano <61257292+Diaszano@users.noreply.github.com>
2024-12-15 22:23:57 -0300

    fix: updated the length type
    the length type was updated from uint32 to int, as it could generate overflow errors

--- strrand.go
+++ strrand.go
@@ -38,64 +38,68 @@ const (
 )
 
 // Binary generates a random string of the specified length using the binary charset (01).
-func Binary(length uint32) string {
+func Binary(length int) string {
 	return random(length, BinaryCharset)
 }
 
 // Octal generates a random string of the specified length using the octal charset (01234567).
-func Octal(length uint32) string {
+func Octal(length int) string {
 	return random(length, OctalCharset)
 }
 
 // Decimal generates a random string of the specified length using the decimal charset (0123456789).
-func Decimal(length uint32) string {
+func Decimal(length int) string {
 	return random(length, DecimalCharset)
 }
 
 // Hexadecimal generates a random string of the specified length using the hexadecimal charset (0123456789abcdef).
-func Hexadecimal(length uint32) string {
+func Hexadecimal(length int) string {
 	return random(length, HexadecimalCharset)
 }
 
 // CapitalLetters generates a random string of the specified length using uppercase letters.
-func CapitalLetters(length uint32) string {
+func CapitalLetters(length int) string {
 	return random(length, UppercaseCharset)
 }
 
 // LowercaseLetters generates a random string of the specified length using lowercase letters.
-func LowercaseLetters(length uint32) string {
+func LowercaseLetters(length int) string {
 	return random(length, LowercaseCharset)
 }
 
 // SpecialLetters generates a random string of the specified length using special characters.
-func SpecialLetters(length uint32) string {
+func SpecialLetters(length int) string {
 	return random(length, SpecialCharset)
 }
 
 // Base62 generates a random string of the specified length using the base62 charset (0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz).
-func Base62(length uint32) string {
+func Base62(length int) string {
 	return random(length, Base62Charset)
 }
 
 // Base64 generates a random string of the specified length using the base64 charset (0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/).
-func Base64(length uint32) string {
+func Base64(length int) string {
 	return random(length, Base64Charset)
 }
 
 // Letters generates a random string of the specified length using both uppercase and lowercase letters.
-func Letters(length uint32) string {
+func Letters(length int) string {
 	return random(length, AlphabetCharset)
 }
 
 // DefaultString generates a random string of the specified length using the default charset (base62 + special characters).
-func DefaultString(length uint32) string {
+func DefaultString(length int) string {
 	return random(length, DefaultCharset)
 }
 
 // random generates a random string of the specified length using the provided charset.
-func random(length uint32, charset string) string {
+func random(length int, charset string) string {
+	if length <= 0 {
+		return ""
+	}
+
 	var buffer ibytes.Buffer
-	buffer.Grow(int(length))
+	buffer.Grow(length)
 
 	charsetRune := []rune(charset)
 	charsetRuneLength := uint32(len(charsetRune))
@@ -109,7 +113,7 @@ func random(length uint32, charset string) string {
 }
 
 // bytes generates a byte slice of the specified length filled with random data.
-func bytes(length uint32) []byte {
+func bytes(length int) []byte {
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	if err != nil {
@@ -120,7 +124,7 @@ func bytes(length uint32) []byte {
 
 // String generates a random string of the specified length using a custom charset if provided,
 // otherwise, it uses the default charset.
-func String(length uint32, customCharset ...string) string {
+func String(length int, customCharset ...string) string {
 	if len(customCharset) == 0 {
 		return DefaultString(length)
 	}
